c347ca3e38a8069c88d09c7063322145acba4b33
Priyanshu Jaiswal <priyanshuskt7839@gmail.com>
2026-03-27 12:50:50 +0530

    host

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000, // optional but good to fix it
-    hmr: {
-      host: '10.164.8.106', // your Mac IP
-    }
-  }
 })
